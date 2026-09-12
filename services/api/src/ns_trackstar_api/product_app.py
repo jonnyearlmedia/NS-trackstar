75668b7d1109657976cf7de1adb55dc921b5c909
jonnyearlmedia <jonny@jonnyearl.com>
2026-09-12 09:20:32 -0700

    chore: remove temporary location diagnostics from API

--- services/api/src/ns_trackstar_api/product_app.py
+++ services/api/src/ns_trackstar_api/product_app.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 from ns_trackstar_api.app import app
-from ns_trackstar_api.diagnostics import router as diagnostics_router
 from ns_trackstar_api.product import router as product_router
 
 app.include_router(product_router)
-app.include_router(diagnostics_router)
