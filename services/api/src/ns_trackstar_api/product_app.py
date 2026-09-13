7c5bf66445184d3e626021a86df091921cc20a20
jonnyearlmedia <jonny@jonnyearl.com>
2026-09-13 00:22:24 -0700

    Register project quality audit API

--- services/api/src/ns_trackstar_api/product_app.py
+++ services/api/src/ns_trackstar_api/product_app.py
@@ -9,6 +9,7 @@ from ns_trackstar_api.freshness import router as freshness_router
 from ns_trackstar_api.lifecycle_audit import router as lifecycle_audit_router
 from ns_trackstar_api.product import router as product_router
 from ns_trackstar_api.public_changes import router as public_changes_router
+from ns_trackstar_api.quality import router as quality_router
 from ns_trackstar_api.search import router as search_router
 from ns_trackstar_api.taxonomy import router as taxonomy_router
 
@@ -21,4 +22,5 @@ app.include_router(lifecycle_audit_router)
 app.include_router(freshness_router)
 app.include_router(categories_router)
 app.include_router(churn_router)
+app.include_router(quality_router)
 app.include_router(search_router)
