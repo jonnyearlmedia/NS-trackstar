37a336179d53f0dec4377ead5bc5da3eba450d49
jonnyearlmedia <jonny@jonnyearl.com>
2026-09-12 22:31:45 -0700

    Load rich content row styles

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./final-device.css";
 import "./visual-refresh.css";
 import "./editorial-polish.css";
 import "./interaction-polish.css";
+import "./content-polish.css";
 
 import { PwaBootstrap } from "@/components/pwa-bootstrap";
 
