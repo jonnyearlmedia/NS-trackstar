1432ec47b4bd32a50149b0abf94a879c78b80757
jonnyearlmedia <jonny@jonnyearl.com>
2026-09-12 19:40:02 -0700

    fix: mount public updates feed

--- services/api/src/ns_trackstar_api/product_app.py
+++ services/api/src/ns_trackstar_api/product_app.py
@@ -8,11 +8,13 @@ from ns_trackstar_api.churn import router as churn_router
 from ns_trackstar_api.freshness import router as freshness_router
 from ns_trackstar_api.lifecycle_audit import router as lifecycle_audit_router
 from ns_trackstar_api.product import router as product_router
+from ns_trackstar_api.public_changes import router as public_changes_router
 from ns_trackstar_api.search import router as search_router
 from ns_trackstar_api.taxonomy import router as taxonomy_router
 
 app.include_router(product_router)
 app.include_router(area_router)
+app.include_router(public_changes_router)
 app.include_router(taxonomy_router)
 app.include_router(cadence_router)
 app.include_router(lifecycle_audit_router)
